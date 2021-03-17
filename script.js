1ac14cd1d313300c64d2a202f056c5b72a8b310f
Arkopravo Das <arko.andrew2000@gmail.com>
2021-03-18 00:33:55 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const APIURL =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-// initially get fav movies
+// initially get favorite movies
 getMovies(APIURL);
 
 async function getMovies(url) {
@@ -21,7 +19,7 @@ async function getMovies(url) {
 }
 
 function showMovies(movies) {
-    // clear main
+    // clear the main
     main.innerHTML = "";
 
     movies.forEach((movie) => {
@@ -31,10 +29,7 @@ function showMovies(movies) {
         movieEl.classList.add("movie");
 
         movieEl.innerHTML = `
-            <img
-                src="${IMGPATH + poster_path}"
-                alt="${title}"
-            />
+            <img src="${IMGPATH + poster_path}" alt="${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getClassByRate(vote_average)}">${vote_average}</span>
